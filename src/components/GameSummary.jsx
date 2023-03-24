de7c9f6ccb8d7708ebc36878f79a4e973c3b7fc0
Nitsan Stark Levit <nitsan1986@gmail.com>
2023-03-24 15:49:54 +0300

    fixed bug in build

--- src/components/GameSummary.jsx
+++ src/components/GameSummary.jsx
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Rule from "./Game/Rule";
-import Character from "./Game/Character";
+// import Character from "./Game/Character";
 
 import { useGame } from "../helpers/GameContext";
 
