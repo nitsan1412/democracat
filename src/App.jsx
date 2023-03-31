676c655e915fb36030e34dd727f8801aafdeb18f
Idan Stark <idanstark42@gmail.com>
2023-03-31 16:29:29 +0300

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ export default function App() {
   return (
     <ThemeProvider>
       <GameProvider>
-        <Menu />
         <Main />
       </GameProvider>
     </ThemeProvider>
--- src/components/StartGameMenu.jsx
+++ src/components/StartGameMenu.jsx
@@ -12,69 +12,17 @@ export default function StartGameMenu() {
     date: localStorage.getItem("highest-score-dateTime"),
   };
   return (
-    <Stack alignItems="center" sx={{ padding: "1rem" }}>
-      <Typography variant="h3" sx={{ fontWeight: "700" }}>
-        DemocraCat
+    <Stack alignItems="center" sx={{ padding: "3rem", height: 1 }}>
+      <Typography variant="h3" sx={{ fontWeight: "700", marginBottom: '-0.5rem' }}>
+        מיציטופיה
       </Typography>
-      <Typography variant="h5" sx={{ fontWeight: "700" }}>
+      <Typography variant="h5">
         למען פלורליזם חברתי
       </Typography>
-      <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-        כמה חתולים יגיעו ליעד
-      </Typography>
-
-      <Stack direction="row" gap={2}>
-        <CharacterImage
-          key="orthodox-man"
-          characterType="orthodox-man"
-          sx={StartGameMenu.characterStyles}
-        />
-        <CharacterImage
-          key="arab-woman"
-          characterType="arab-woman"
-          sx={StartGameMenu.characterStyles}
-        />
+      <Stack sx={{ flexGrow: 1, '*': { fontSize: '1.2rem', height: "3rem" }, width: 0.6 }} gap={1} justifyContent="center">
+        <Button fullWidth disableElevation variant="contained" color="primary">למשחק</Button>
+        <Button fullWidth disableElevation variant="outlined" color="secondary" sx={{ borderWidth: 2 }}>הוראות</Button>
       </Stack>
-      {localStorage.getItem("highest-score") && (
-        <Stack direction="row" gap={2}>
-          <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-            הניקוד הגבוה ביותר שלך:
-          </Typography>
-          <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-            {highestScoreToShow.highestScore}
-          </Typography>
-        </Stack>
-      )}
-      {highestScoreToShow.highestScore && (
-        <Stack direction="row" gap={2}>
-          <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-            מתאריך:{" "}
-          </Typography>
-          <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-            {highestScoreToShow.date}
-          </Typography>
-        </Stack>
-      )}
-      <Stack direction="row" gap={2}>
-        <CharacterImage
-          key="secular-woman"
-          characterType="secular-woman"
-          sx={StartGameMenu.characterStyles}
-        />
-        <CharacterImage
-          key="religious-man"
-          characterType="religious-man"
-          sx={StartGameMenu.characterStyles}
-        />
-      </Stack>
-
-      <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-        מי יצליח ליצר מגון רחב של מגזרים ומגדרים שונים
-      </Typography>
-
-      <Button variant="contained" fullWidth onClick={start}>
-        להתחיל
-      </Button>
     </Stack>
   );
 }
