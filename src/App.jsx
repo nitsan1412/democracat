c49fb335b42758c5f4dd3cbf3cb94746076bfa77
Nitsan Stark Levit <nitsan1986@gmail.com>
2023-03-31 16:19:16 +0300

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 import { GameProvider } from "./helpers/GameContext";
 import ThemeProvider from "./helpers/Theme";
 
-import Menu from "./components/Menu";
-import Main from "./components/Main";
+import StartGameMenu from "./components/StartGameMenu";
+import GameSummary from "./components/GameSummary";
+import Game from "./components/Game";
 
 export default function App() {
-
   return (
     <ThemeProvider>
       <GameProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<StartGameMenu />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/summery" element={<GameSummary />} />
+          </Routes>
+        </BrowserRouter>
+      </GameProvider>
+      {/* <GameProvider>
         <Menu />
         <Main />
-      </GameProvider>
+      </GameProvider> */}
     </ThemeProvider>
   );
 }
