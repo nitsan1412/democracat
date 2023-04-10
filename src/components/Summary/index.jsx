12090aa5bb11e958cd02ace31acde8809f8802e4
Nitsan Stark Levit <nitsan1986@gmail.com>
2023-04-10 22:26:12 +0300

    added sticky footer in summery page

--- src/components/Summary/index.jsx
+++ src/components/Summary/index.jsx
@@ -21,36 +21,57 @@ export default function GameSummary() {
   }, [game.status, navigate]);
   if (game.status === "pending") return <></>;
   return (
-    <Stack
-      alignItems="center"
-      justifyContent="center"
-      direction="column"
-      gap={1}
-      sx={{
-        padding: "1rem",
-        ".slick-slide": { padding: "0.5rem 1rem" },
-        ".slick-arrow": { zIndex: 72, "&::before": { color: "#000000" } },
-        ".slick-prev": { left: "-45px" },
-        ".slick-next": { right: "-55px" },
-      }}
-    >
-      <SummeryHeader />
-      <Stack sx={boxStyle}>
-        <EndScore />
-      </Stack>
-      <Stack sx={boxStyle}>
-        <SummeryReview />
-      </Stack>
-      <Stack sx={boxStyle}>
-        <SummeryRulesReview />
+    <>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        gap={1}
+        sx={{
+          padding: "1rem",
+          ".slick-slide": { padding: "0.5rem 1rem" },
+          ".slick-arrow": { zIndex: 72, "&::before": { color: "#000000" } },
+          ".slick-prev": { left: "-45px" },
+          ".slick-next": { right: "-55px" },
+        }}
+      >
+        <SummeryHeader />
+        <Stack sx={boxStyle}>
+          <EndScore />
+        </Stack>
+        <Stack sx={boxStyle}>
+          <SummeryReview />
+        </Stack>
+        <Stack sx={boxStyle}>
+          <SummeryRulesReview />
+        </Stack>
       </Stack>
 
       <Stack
         gap={2}
-        sx={{ flexDirection: "row", justifyContent: "space-between" }}
+        sx={{
+          flexDirection: "row",
+          // position: "-webkit-sticky",
+          position: "sticky",
+          bottom: "0",
+          height: "6rem",
+          width: "100%",
+          zIndex: 150,
+          justifyContent: "center",
+          paddingBottom: "2rem",
+          paddingTop: "2rem",
+          alignItems: "center",
+          backgroundColor: "white",
+          borderTop: "1px solid #F4F4F4",
+        }}
       >
         <ShareButton
-          sx={{ fontSize: "0.9rem", fontWeight: "500", padding: "0.3rem 1rem" }}
+          sx={{
+            fontSize: "0.9rem",
+            fontWeight: "500",
+            height: "2.7rem",
+            padding: "0.5rem 1rem",
+          }}
         />
         <Stack
           gap={1}
@@ -62,19 +83,23 @@ export default function GameSummary() {
             alignSelf: "center",
             justifyContent: "center",
             borderRadius: "12px",
+            // backgroundColor: "white",
           }}
           onClick={() => navigate("/")}
         >
           <img src={restartGameArrow} alt="" width="15px" height="15px" />
           <Typography
             variant="subtitle1"
-            sx={{ fontSize: "0.9rem", fontWeight: "500" }}
+            sx={{
+              fontSize: "0.9rem",
+              fontWeight: "500",
+            }}
           >
             שחק שוב
           </Typography>
         </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
