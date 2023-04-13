52163d8c6127770a47743b1449952fb0b1726781
Idan Stark <idanstark42@gmail.com>
2023-04-13 12:14:54 +0300

    Menu positioning

--- src/components/Game/Menu.jsx
+++ src/components/Game/Menu.jsx
@@ -10,47 +10,44 @@ import backArrow from "../../images/icons/backArrow.svg";
 
 export default function Menu() {
   const { newGame } = useGame();
-
   const navigate = useNavigate();
 
   return (
     <Stack
       position="static"
-      sx={{ "+*": { height: "calc(100% - 48px)" } }}
       direction="row"
       alignItems="center"
       justifyContent="space-around"
-      marginTop="1rem"
-      minWidth="100%"
+      sx={{ width: "100%", height: "4rem", "+*": { height: "calc(100% - 4rem)" } }}
     >
-      <Stack
-        alignItems="center"
-        justifyContent="center"
-        sx={{ height: "3rem", cursor: "pointer" }}
+    <Stack
+        direction="row"
+        gap={1}
+        onClick={() => {
+          newGame();
+          navigate("/");
+        }}
       >
-        <Stack
-          direction="row"
-          gap={1}
-          onClick={() => {
-            newGame();
-            navigate("/");
-          }}
-        >
-          <img src={backArrow} alt="" />
-
-          <Typography variant="subtitle1" sx={{ fontWeight: "700" }}>
-            חזרה
-          </Typography>
-        </Stack>
+        <img src={backArrow} alt="" />
+        <Typography variant="subtitle1" sx={{  }}>
+          חזרה
+        </Typography>
       </Stack>
-      <Chip label={<Score />} variant="outlined" sx={Menu.styles.button} />
-      <Chip label={<Timer />} variant="outlined" sx={Menu.styles.button} />
+      <Stack
+         direction="row"
+         gap={1}
+      >
+        <Chip label={<Score />} variant="outlined" sx={Menu.styles.button} />
+        <Chip label={<Timer />} variant="outlined" sx={Menu.styles.button} />
+      </Stack>
     </Stack>
   );
 }
 
 Menu.styles = {
   button: {
+    height: "2.5rem",
     borderColor: "#ECECEC",
+    borderRadius: "1.25rem"
   },
 };
