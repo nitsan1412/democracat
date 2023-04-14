6ebc9049ec9bebb069b2cde8affb9c9c1135385f
stark526 <tomer526@gmail.com>
2023-04-14 10:38:33 +0300

    fix: build

--- src/components/Game/Menu.jsx
+++ src/components/Game/Menu.jsx
@@ -2,6 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { useGame } from "../../helpers/GameContext";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { Chip } from "@mui/material";
+import Timer from "./Timer";
+import Score from "./Score";
 import backArrow from "../../images/icons/backArrow.svg";
 
 export default function Menu() {
@@ -10,32 +13,30 @@ export default function Menu() {
 
   return (
     <Stack
-      position="static"
-      direction="row"
-      alignItems="center"
-      justifyContent="space-around"
+      position='static'
+      direction='row'
+      alignItems='center'
+      justifyContent='space-around'
       sx={{
         width: "100%",
         height: "4rem",
         "+*": { height: "calc(100% - 4rem)" },
-      }}
-    >
+      }}>
       <Stack
-        direction="row"
+        direction='row'
         gap={1}
         onClick={() => {
           newGame();
           navigate("/");
-        }}
-      >
-        <img src={backArrow} alt="" />
-        <Typography variant="subtitle1" sx={{}}>
+        }}>
+        <img src={backArrow} alt='' />
+        <Typography variant='subtitle1' sx={{}}>
           חזרה
         </Typography>
       </Stack>
-      <Stack direction="row" gap={1}>
-        <Chip label={<Score />} variant="outlined" sx={Menu.styles.button} />
-        <Chip label={<Timer />} variant="outlined" sx={Menu.styles.button} />
+      <Stack direction='row' gap={1}>
+        <Chip label={<Score />} variant='outlined' sx={Menu.styles.button} />
+        <Chip label={<Timer />} variant='outlined' sx={Menu.styles.button} />
       </Stack>
     </Stack>
   );
